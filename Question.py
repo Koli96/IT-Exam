78b1a8f1579e0e2abd5da835e0940b7c6a05d85e
Koli96 <96koli1996@gmail.com>
2018-01-22 20:50:08 +0100

    Add Question API module

---
+++ Question.py
@@ -0,0 +1,104 @@
+import json
+import random
+import copy
+
+class Question:
+    def __init__(self,database):
+        self.__data = json.load(database)
+        self.__questions = copy.deepcopy(self.__data)
+        self.__question = None
+
+    def drawQuestion(self, fromDb = False):
+        if fromDb:
+            self.__question = random.choice(self.__data["questions"])
+        else:
+            if len(self.__questions["questions"]) > 0:
+                idx = random.randrange(len(self.__questions["questions"]))
+                self.__question = self.__questions["questions"][idx]
+                del self.__questions["questions"][idx]
+            else:
+                raise IndexError("There is no more questions to pick!")
+
+    def getAllQuestions(self):
+        questions = []
+        for i in self.__data["questions"]:
+            questions.append(i["question"])
+        return questions
+
+    def getRemainingQuestions(self):
+        questions = []
+        for i in self.__questions["questions"]:
+            questions.append(i["question"])
+        return questions
+
+    def getCount(self):
+        return len(self.__data["questions"])
+
+    def getRemainingCount(self):
+        return len(self.__questions["questions"])
+
+    def pickQuestionAt(self, idx):
+        self.__question = self.__data["questions"][idx]
+
+    def getQuestion(self):
+        return self.__question["question"]
+
+    def getCurrentIndex(self):
+        return self.__data["questions"].index(self.__question)
+
+    def getAllAnswers(self):
+        answers = []
+        for i in self.__question["answers"]:
+            answers.append(i["text"])
+        return answers
+
+    def getTrueAnswers(self):
+        answers = []
+        for i in self.__question["answers"]:
+            if i["isTrue"]:
+                answers.append(i["text"])
+        return answers
+
+    def getTrueAnswersFor(self,idx):
+        answers = []
+        for i in self.__data["questions"][idx]["answers"]:
+            if i["isTrue"]:
+                answers.append(i["text"])
+        return answers
+
+    def getAllAnswersFor(self,idx):
+        answers = []
+        for i in self.__data["questions"][idx]["answers"]:
+            answers.append(i["text"])
+        return answers
+        
+
+    def checkIfAnswerIsTrue(self,answer):
+        for i in self.__question["answers"]:
+            if(i["text"] == answer):
+                return i["isTrue"]
+        return False
+
+    def checkIfAnswerIdIsTrue(self,answerId):
+        try:
+            if int(answerId) >= len(self.__question["answers"]):
+                return False
+        except ValueError:
+            return False
+
+        return self.__question["answers"][int(answerId)]["isTrue"]
+
+    def checkIfAnswerIsTrueFor(self,answer,idx):
+        for i in self.__data["questions"][idx]["answers"]:
+            if(i["text"] == answer):
+                return i["isTrue"]
+        return False
+
+    def checkIfAnswerIdIsTrueFor(self,answerId,idx):
+        try:
+            if int(answerId) >= len(self.__data["questions"][idx]["answers"]):
+                return False
+        except ValueError:
+            return False
+
+        return self.__question["answers"][int(answerId)]["isTrue"]
