5e6a118a7d8bab0bcd242b2b49cf12ca871212de
Damian Nurzyński <damiannnurzynski@gmail.com>
2018-01-23 09:35:54 +0100

    Add new features

--- main.py
+++ main.py
@@ -6,37 +6,53 @@ from Question import *
 from tkinter import *
 from Answer import *
 import codecs
-file = codecs.open('./db.json', 'r', 'utf-8')
 
-quest = Question(file)
+# file = codecs.open('./db.json', 'r', 'utf-8')
+
+# quest = Question(codecs.open('./db.json', 'r', 'utf-8'))
 
 window = Tk()
 window.title("Quiz")
 window.geometry("520x900")
+
+
 # window.wm_resizable(0, 0)
 
 
 def finish():
-    label = Label(navbar, text=str(summarize()['points']) + "/" + str(summarize()['availablePoints']), fg="black", bg="#fff", width=12)
+    print(quest.getAllAvailablePoints())
+    points = Label(navbar, text='Liczba \n punktów:', fg="black", bg="#d3d3d3", width=12)
+    points.config(font=("Arial", 12))
+    points.pack(anchor=E)
+    points.place(y=10)
+
+    label = Label(navbar, text=str(summarize()['points']) + "/" + str(summarize()['availablePoints']), fg="black",
+                  bg="#E91E63", width=12)
     label.config(font=("Arial", 12))
     label.pack(anchor=CENTER)
     label.place(y=60)
+
+    if summarize()['points'] / summarize()['availablePoints'] > 0.5:
+        label.configure(bg='#8BC34A')
+
+
+def buildView():
+    global quest
+    global answersCollections
+    answersCollections = []
+    quest = Question(codecs.open('./db.json', 'r', 'utf-8'))
+    createQuestionsView(column, row, w, answersCollections, window, quest, navbar)
 
 
 
 navbar = Frame(window, bg="#d3d3d3", width=100)
 navbar.pack(fill=Y, side=LEFT)
 
-points = Label(navbar, text='Liczba \n punktów:', fg="black", bg="#d3d3d3", width=12)
-points.config(font=("Arial", 12))
-points.pack(anchor=E)
-points.place(y=10)
+startButton = Button(navbar, text="Start", width=12, command=buildView)
+startButton.pack()
+startButton.place(x=4, y=120)
 
-# startButton = Button(navbar, text="Start", width=12)
-# startButton.pack()
-# startButton.place(x=4, y=120)
-
-endButton = Button(navbar, text="Zakończ quiz", width=12, command=finish)
+endButton = Button(navbar, text="Zakończ", width=12, command=finish)
 endButton.pack()
 endButton.place(x=4, y=150)
 
@@ -44,13 +60,10 @@ exitButton = Button(navbar, text="Wyjdź", width=12, command=window.destroy)
 exitButton.pack()
 exitButton.place(x=4, y=180)
 
-
-
-
 labelFrame = LabelFrame(window)
 labelFrame.pack(side="left", fill=BOTH)
 canvas = Canvas(labelFrame)
-canvas.configure(scrollregion=(0, 0, 900, 2000))
+canvas.configure(scrollregion=(0, 0, 900, 2500))
 
 scrollbar = Scrollbar(labelFrame)
 scrollbar.config(command=canvas.yview)
@@ -62,58 +75,69 @@ frame = Frame(canvas)
 frame.pack(side="top", fill=BOTH)
 canvas.create_window(0, 0, window=frame, anchor="nw", width=800)
 
-answersCollections = []
+# answersCollections = []
 
 column = 1
 row = 0
 w = 0
-for i in range(10):
-    question = Frame(frame, bg="#fff")
 
-    row += 3
-    quest.drawQuestion()
-    label = Label(question, text=str(i + 1) + ". " + quest.getQuestion(), fg="black", bg="#fff")
-    label.configure(wraplength=350, justify=LEFT)
-    label.config(font=("Arial", 12))
-    label.pack(anchor=W, pady=5, padx=15)
+test = Frame(labelFrame, width=15)
+test.pack(fill=Y, side=RIGHT)
 
-    answerCollection = AnswerCollection(quest.getCurrentIndex(), quest)
-    answersCollections.append(answerCollection)
-    stringVar = None
-    row += 3
 
-    for answer in quest.getAllAnswersFor(quest.getCurrentIndex()):
-        row += 1
-        w += 1
+def createQuestionsView(column, row, w, answersCollections, window, quest, navbar):
+    for widget in frame.winfo_children():
+        widget.destroy()
 
-        if True == quest.isMultiAnswer():
-            answerObject = Answer(answer, IntVar())
-            checkButton = Checkbutton(question, text=answer, variable=answerObject.getVariable(), bg="#fff")
-            checkButton.pack(anchor=W, padx=30)
-            checkButton.configure(wraplength=350, justify=LEFT)
-        else:
-            if stringVar == None:
-                stringVar = StringVar()
-                stringVar.set("1")
-            answerObject = Answer(answer, stringVar)
-            radioButton = Radiobutton(question, text=answer, variable=answerObject.getVariable(), value=answer,
-                                      bg="#fff")
-            radioButton.pack(anchor=W, padx=30)
-            radioButton.configure(wraplength=350, justify=LEFT)
+    for widget in navbar.winfo_children():
+        print(widget)
+        if str(widget).find('label') != -1:
+            widget.destroy()
 
-        answerCollection.add(answerObject)
-    question.pack(fill=BOTH, padx=8, pady=10)
+
+
+    for i in range(10):
+        question = Frame(frame, bg="#fff")
+
+        row += 3
+        quest.drawQuestion()
+        label = Label(question, text=str(i + 1) + ". " + quest.getQuestion(), fg="black", bg="#fff")
+        label.configure(wraplength=350, justify=LEFT)
+        label.config(font=("Arial", 12))
+        label.pack(anchor=W, pady=5, padx=15)
+
+        answerCollection = AnswerCollection(quest.getCurrentIndex(), quest)
+        answersCollections.append(answerCollection)
+        stringVar = None
+        row += 3
+
+        for answer in quest.getAllAnswersFor(quest.getCurrentIndex()):
+            row += 1
+            w += 1
+
+            if True == quest.isMultiAnswer():
+                answerObject = Answer(answer, IntVar())
+                checkButton = Checkbutton(question, text=answer, variable=answerObject.getVariable(), bg="#fff")
+                checkButton.pack(anchor=W, padx=30)
+                checkButton.configure(wraplength=300, justify=LEFT)
+            else:
+                if stringVar == None:
+                    stringVar = StringVar()
+                    stringVar.set("1")
+                answerObject = Answer(answer, stringVar)
+                radioButton = Radiobutton(question, text=answer, variable=answerObject.getVariable(), value=answer,
+                                          bg="#fff")
+                radioButton.pack(anchor=W, padx=30)
+                radioButton.configure(wraplength=300, justify=LEFT)
+
+            answerCollection.add(answerObject)
+        question.pack(fill=BOTH, padx=8, pady=10)
 
 
 def summarize():
     summarize = PointManager.summarize(answersCollections, quest)
-    # print(summarize)
+    # print(answersCollections)
     return summarize
 
-
-
-
-test = Frame(labelFrame, width=15)
-test.pack(fill=Y, side=RIGHT)
 
 window.mainloop()
